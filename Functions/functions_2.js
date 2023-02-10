e1a18249c45ddab698da7f050419d66f1006d0aa
Thiago Oliveira <72051208+Thiolivs@users.noreply.github.com>
2023-02-09 23:48:29 -0300

    Add files via Upload

---
+++ Functions/functions_2.js
@@ -0,0 +1,19 @@
+let dobble = function(a){
+    return 2*a
+}
+
+function dobble2(a){
+    return 2*a
+}
+
+let dobble3 = (a) => { 
+    return 2*a 
+}
+
+let dobble4 = a => 2*a
+
+
+console.log(dobble(3)) 
+console.log(dobble2(3))
+console.log(dobble3(3))
+console.log(dobble4(3))

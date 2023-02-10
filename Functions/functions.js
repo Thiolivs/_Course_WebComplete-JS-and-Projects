6e023dbea5acb8ec4c10afbb60dbf40e24786025
thiolivs <thiolivs@gmail.com>
2023-02-09 23:43:00 -0300

    Functions

---
+++ Functions/functions.js
@@ -0,0 +1,15 @@
+let dobble = function(a){
+    return 2*a
+}
+
+function dobble2(a){
+    return 2*a
+}
+
+let dobble3 = (a) => { 
+    return 2*a 
+}
+
+let dobble4 = a => 2*a
+
+console.log(dobble(5))
